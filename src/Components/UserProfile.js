3d62473df90cde5fe8823ab2f17ee5ee8de05fea
jaydeepparmar2244 <jaydeep2244.67@gmail.com>
2022-04-30 16:07:17 +0530

    showing results of all exams taken by particular user in profile section!

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -3,185 +3,172 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 export const UserProfile = () => {
-    var userId = useParams().userId;
-    const [userDetail, setuserDetail] = useState([])
-    const getUserDetail = () =>{
-        axios.get(`http://localhost:8080/users/${userId}`).then(res=>{
-            console.log(res.data.data)
-            setuserDetail(res.data.data)
-        }).catch(err=>{
-            console.log(err)
-        })
-    }
+  var userId = useParams().userId;
+  const [userDetail, setuserDetail] = useState([])
+  const [resultList, setresultList] = useState([])
+  const getUserDetail = () => {
+    axios.get(`http://localhost:8080/users/${userId}`).then(res => {
+      console.log(res.data.data)
+      setuserDetail(res.data.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
 
-    useEffect(() => {
-      getUserDetail()
-    }, [])
-    
+  const getResultsOfUser = () => {
+    axios.get(`http://localhost:8080/results/${userId}`).then(res => {
+      console.log(res.data.data)
+      setresultList(res.data.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    getUserDetail();
+    getResultsOfUser();
+  }, [])
+
   return (
- <section style={{backgroundColor: '#eee'}}>
-  <div className="container py-5">
-    <div className="row">
-      <div className="col">
-        <nav aria-label="breadcrumb" className="bg-light rounded-3 p-3 mb-4">
-          <ol className="breadcrumb mb-0">
-            <li className="breadcrumb-item"><a href="#">Home</a></li>
-            <li className="breadcrumb-item"><a href="#">User</a></li>
-            <li className="breadcrumb-item active" aria-current="page">User Profile</li>
-          </ol>
-        </nav>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-lg-4">
-        <div className="card mb-4">
-          <div className="card-body text-center">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar" className="rounded-circle img-fluid" style={{width: 150}} />
-            <h5 className="my-3">{userDetail.firstName}</h5>
-            <p className="text-muted mb-1">{localStorage.getItem('role')}</p>
-            <p className="text-muted mb-4">Bay Area, San Francisco, CA</p>
-            <div className="d-flex justify-content-center mb-2">
-              <button type="button" className="btn btn-primary">Follow</button>
-              <button type="button" className="btn btn-outline-primary ms-1">Message</button>
-            </div>
-          </div>
-        </div>
-        <div className="card mb-4 mb-lg-0">
-          <div className="card-body p-0">
-            <ul className="list-group list-group-flush rounded-3">
-              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i className="fas fa-globe fa-lg text-warning" />
-                <p className="mb-0">https://mdbootstrap.com</p>
-              </li>
-              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i className="fab fa-github fa-lg" style={{color: '#333333'}} />
-                <p className="mb-0">mdbootstrap</p>
-              </li>
-              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i className="fab fa-twitter fa-lg" style={{color: '#55acee'}} />
-                <p className="mb-0">@mdbootstrap</p>
-              </li>
-              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i className="fab fa-instagram fa-lg" style={{color: '#ac2bac'}} />
-                <p className="mb-0">mdbootstrap</p>
-              </li>
-              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i className="fab fa-facebook-f fa-lg" style={{color: '#3b5998'}} />
-                <p className="mb-0">mdbootstrap</p>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="col-lg-8">
-        <div className="card mb-4">
-          <div className="card-body">
-            <div className="row">
-              <div className="col-sm-3">
-                <p className="mb-0">Full Name</p>
-              </div>
-              <div className="col-sm-9">
-                <p className="text-muted mb-0">{userDetail.firstName} {userDetail.lastName}</p>
-              </div>
-            </div>
-            <hr />
-            <div className="row">
-              <div className="col-sm-3">
-                <p className="mb-0">Email</p>
-              </div>
-              <div className="col-sm-9">
-                <p className="text-muted mb-0">{userDetail.email}</p>
-              </div>
-            </div>
-            <hr />
-            <div className="row">
-              <div className="col-sm-3">
-                <p className="mb-0">Gender</p>
-              </div>
-              <div className="col-sm-9">
-                <p className="text-muted mb-0">{userDetail.gender}</p>
-              </div>
-            </div>
-            <hr />
-            <div className="row">
-              <div className="col-sm-3">
-                <p className="mb-0">Mobile</p>
-              </div>
-              <div className="col-sm-9">
-                <p className="text-muted mb-0">(098) 765-4321</p>
-              </div>
-            </div>
-            <hr />
-            <div className="row">
-              <div className="col-sm-3">
-                <p className="mb-0">Address</p>
-              </div>
-              <div className="col-sm-9">
-                <p className="text-muted mb-0">Bay Area, San Francisco, CA</p>
-              </div>
-            </div>
+    <section style={{ backgroundColor: '#eee' }}>
+      <div className="container py-5">
+        <div className="row">
+          <div className="col">
+            <nav aria-label="breadcrumb" className="bg-light rounded-3 p-3 mb-4">
+              <ol className="breadcrumb mb-0">
+                <li className="breadcrumb-item"><a href="#">Home</a></li>
+                <li className="breadcrumb-item"><a href="#">User</a></li>
+                <li className="breadcrumb-item active" aria-current="page">User Profile</li>
+              </ol>
+            </nav>
           </div>
         </div>
         <div className="row">
-          <div className="col-md-6">
-            <div className="card mb-4 mb-md-0">
-              <div className="card-body">
-                <p className="mb-4"><span className="text-primary font-italic me-1">assigment</span> Project Status</p>
-                <p className="mb-1" style={{fontSize: '.77rem'}}>Web Design</p>
-                <div className="progress rounded" style={{height: 5}}>
-                  <div className="progress-bar" role="progressbar" style={{width: '80%'}} aria-valuenow={80} aria-valuemin={0} aria-valuemax={100} />
-                </div>
-                <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Website Markup</p>
-                <div className="progress rounded" style={{height: 5}}>
-                  <div className="progress-bar" role="progressbar" style={{width: '72%'}} aria-valuenow={72} aria-valuemin={0} aria-valuemax={100} />
-                </div>
-                <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>One Page</p>
-                <div className="progress rounded" style={{height: 5}}>
-                  <div className="progress-bar" role="progressbar" style={{width: '89%'}} aria-valuenow={89} aria-valuemin={0} aria-valuemax={100} />
-                </div>
-                <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Mobile Template</p>
-                <div className="progress rounded" style={{height: 5}}>
-                  <div className="progress-bar" role="progressbar" style={{width: '55%'}} aria-valuenow={55} aria-valuemin={0} aria-valuemax={100} />
-                </div>
-                <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Backend API</p>
-                <div className="progress rounded mb-2" style={{height: 5}}>
-                  <div className="progress-bar" role="progressbar" style={{width: '66%'}} aria-valuenow={66} aria-valuemin={0} aria-valuemax={100} />
+          <div className="col-lg-4">
+            <div className="card mb-4">
+              <div className="card-body text-center">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar" className="rounded-circle img-fluid" style={{ width: 150 }} />
+                <h5 className="my-3">{userDetail.firstName}</h5>
+                <p className="text-muted mb-1">{localStorage.getItem('role')}</p>
+                <p className="text-muted mb-4">Bay Area, San Francisco, CA</p>
+                <div className="d-flex justify-content-center mb-2">
+                  <button type="button" className="btn btn-primary">Follow</button>
+                  <button type="button" className="btn btn-outline-primary ms-1">Message</button>
                 </div>
               </div>
             </div>
+            <div className="card mb-4 mb-lg-0">
+              <div className="card-body p-0">
+                <ul className="list-group list-group-flush rounded-3">
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i className="fas fa-globe fa-lg text-warning" />
+                    <p className="mb-0">https://mdbootstrap.com</p>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i className="fab fa-github fa-lg" style={{ color: '#333333' }} />
+                    <p className="mb-0">mdbootstrap</p>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i className="fab fa-twitter fa-lg" style={{ color: '#55acee' }} />
+                    <p className="mb-0">@mdbootstrap</p>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i className="fab fa-instagram fa-lg" style={{ color: '#ac2bac' }} />
+                    <p className="mb-0">mdbootstrap</p>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <i className="fab fa-facebook-f fa-lg" style={{ color: '#3b5998' }} />
+                    <p className="mb-0">mdbootstrap</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="col-md-6">
-            <div className="card mb-4 mb-md-0">
+          <div className="col-lg-8">
+            <div className="card mb-4">
               <div className="card-body">
-                <p className="mb-4"><span className="text-primary font-italic me-1">assigment</span> Project Status</p>
-                <p className="mb-1" style={{fontSize: '.77rem'}}>Web Design</p>
-                <div className="progress rounded" style={{height: 5}}>
-                  <div className="progress-bar" role="progressbar" style={{width: '80%'}} aria-valuenow={80} aria-valuemin={0} aria-valuemax={100} />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Full Name</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">{userDetail.firstName} {userDetail.lastName}</p>
+                  </div>
                 </div>
-                <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Website Markup</p>
-                <div className="progress rounded" style={{height: 5}}>
-                  <div className="progress-bar" role="progressbar" style={{width: '72%'}} aria-valuenow={72} aria-valuemin={0} aria-valuemax={100} />
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Email</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">{userDetail.email}</p>
+                  </div>
                 </div>
-                <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>One Page</p>
-                <div className="progress rounded" style={{height: 5}}>
-                  <div className="progress-bar" role="progressbar" style={{width: '89%'}} aria-valuenow={89} aria-valuemin={0} aria-valuemax={100} />
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Gender</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">{userDetail.gender}</p>
+                  </div>
                 </div>
-                <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Mobile Template</p>
-                <div className="progress rounded" style={{height: 5}}>
-                  <div className="progress-bar" role="progressbar" style={{width: '55%'}} aria-valuenow={55} aria-valuemin={0} aria-valuemax={100} />
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Mobile</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">(098) 765-4321</p>
+                  </div>
                 </div>
-                <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Backend API</p>
-                <div className="progress rounded mb-2" style={{height: 5}}>
-                  <div className="progress-bar" role="progressbar" style={{width: '66%'}} aria-valuenow={66} aria-valuemin={0} aria-valuemax={100} />
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <p className="mb-0">Address</p>
+                  </div>
+                  <div className="col-sm-9">
+                    <p className="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="card mb-12 mb-md-0">
+                  <div className="card-body">
+                    <p className="mb-12"><span className="text-primary font-italic me-1">Results</span> Project Status</p>
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">Exam Title</th>
+                          <th scope="col">Subject</th>
+                          <th scope="col">Total Marks</th>
+                          <th scope="col">Your Marks</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {resultList.map(result => {
+                          return (
+                            <tr>
+                              <th scope="row">{result.exam.examName}</th>
+                              <td>{result.exam.subject.subjectName}</td>
+                              <td>{result.exam.totalMarks}</td>
+                              <td>{result.marks}</td>
+                              {/* <td>@mdo</td> */}
+                            </tr>
+                          )
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
 
   )
 }
